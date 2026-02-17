373ae5bf42384b094b788ea87e61e4dcc86830da
Amir Shaikh <skamir2410@gmail.com>
2026-02-17 21:42:10 +0530

    Deployed

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,5 @@
-// const BASE_URL = 'https://innovative-staffing-1.onrender.com/api';
 import axios from 'axios';
-// const BASE_URL = 'http://localhost:5000/api';
-const BASE_URL = 'https://innovativesolution-api.onrender.com/api';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true
