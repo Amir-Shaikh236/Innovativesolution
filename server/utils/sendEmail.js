c3325e5929e3317d4f99e647ac8a62a13e70b8a9
Amir Shaikh <skamir2410@gmail.com>
2026-02-18 21:56:20 +0530

    Again Testing

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,12 +1,23 @@
 const nodemailer = require('nodemailer');
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
+  }
 });
+
 
 const sendVerificationEmail = async (email, token) => {
   try {
